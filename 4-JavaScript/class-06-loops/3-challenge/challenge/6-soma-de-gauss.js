718e86dd556577ddb59524a9c19bafa9d9838573
Eduardo gonçalves <eduardogoncalves3692@gmail.com>
2023-10-23 18:50:37 -0300

    add examples find-filter

--- 4-JavaScript/class-06-loops/3-challenge/challenge/6-soma-de-gauss.js
+++ 4-JavaScript/class-06-loops/3-challenge/challenge/6-soma-de-gauss.js
@@ -1,8 +1,12 @@
+
+
 function somaDeGauss(n) {
     return (n * (n + 1)) / 2;
 }
 
+
+
 // Exemplo de uso
-const n = 2; // substitua por qualquer número natural que desejar
+const n = 8; // coloque um número natural
 const soma = somaDeGauss(n);
 document.write(`A soma do número n com seus antecessores á ${n} tem a soma ${soma}.`);
