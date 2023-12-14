d12c836e5c609ca3baf8f012482174bb60beeabd
Eduardo gonçalves <eduardogoncalves3692@gmail.com>
2023-12-14 19:37:12 -0300

    add styles d types

--- 5-React/class-05-css-in-js/src/pages/App.tsx
+++ 5-React/class-05-css-in-js/src/pages/App.tsx
@@ -2,11 +2,12 @@ import { Button } from "../components/Button";
 import { AppContainer } from "./styles";
 
 export function App() {
-  <>
-    <AppContainer>
-    <h1>CSS in JS</h1>
-    </AppContainer>
-    <Button />
-
-  </>;
+  return (
+    <>
+      <AppContainer>
+        <h1>CSS in JS</h1>
+        <Button />
+      </AppContainer>
+    </>
+  );
 }
--- 5-React/class-05-css-in-js/src/pages/styles.ts
+++ 5-React/class-05-css-in-js/src/pages/styles.ts
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
 export const AppContainer = styled.div`
-display: grid;
-place-content: center;
-height: 100vh;
-background: ${({ theme }) => theme.colors.backgroundPage};
-color: ${({theme}) => theme.colors.text};
+  display: grid;
+  place-content: center;
+  height: 100vh;
+  background: ${({ theme }) => theme.colors.backgroundPage};
+  color: ${({ theme }) => theme.colors.text};
 `;
