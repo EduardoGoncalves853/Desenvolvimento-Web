a189b95f13dc96331b11c728910106cc97534f75
Eduardo Gonçalves <eduardogoncalves3693@gmail.com>
2024-03-11 19:01:35 -0300

    add details SQL

--- 4-JavaScript/class-05-conditional/error.catch/challenge/3-convert-temperature.js
+++ 4-JavaScript/class-05-conditional/error.catch/challenge/3-convert-temperature.js
@@ -1,36 +1,36 @@
+/** @format */
+
 // fahrenheit === C * 9/5 + 32;
-// Celsius === F *  
+// Celsius === F *
 
 // entrada
 
 function degreeConvert(degree) {
-    const degreeAux = degree.split(" ").join("");
+  const degreeAux = degree.split(" ").join("");
 
-    const arrayDegree = Array.from(degreeAux);
-    const scaleDegree = arrayDegree.pop().toUpperCase();
-    const newDegree = Number(arrayDegree.join(""));
+  const arrayDegree = Array.from(degreeAux);
+  const scaleDegree = arrayDegree.pop().toUpperCase();
+  const newDegree = Number(arrayDegree.join(""));
 
-    if (isNaN(newDegree)) throw new error("Valor Inválido!")
+  if (isNaN(newDegree)) throw new error("Valor Inválido!");
 
-    function convertCelsiusToFahrenheit() {
-        return (newDegree * 9)/5 + 32;
-    }
+  function convertCelsiusToFahrenheit() {
+    return (newDegree * 9) / 5 + 32;
+  }
 
-    
-    function convertFahrenheitToCelsius() {
-        return ((newDegree - 32) * 5) / 9;
-    }
+  function convertFahrenheitToCelsius() {
+    return ((newDegree - 32) * 5) / 9;
+  }
 
-
-    switch (scaleDegree) {
-        case "C":
-            return convertCelsiusToFahrenheit() + "F";
-        case "F":
-            return convertFahrenheitToCelsius() + "C";
-        default :
-            throw new error ("Escala não identificada")
-        }
-    }
+  switch (scaleDegree) {
+    case "C":
+      return convertCelsiusToFahrenheit() + "F";
+    case "F":
+      return convertFahrenheitToCelsius() + "C";
+    default:
+      throw new error("Escala não identificada");
+  }
+}
 
 console.log(degree);
 // graus
@@ -39,15 +39,13 @@ console.log(degreeAux);
 console.log(arraydegree);
 console.log(scaleDegree);
 console.log(newDegree);
-    
-
 
 // saída
 
 try {
-    const temperature = "-459F   "
-    const temperatureConverted = degreeConvert(temperature);
-    console.log(`${temperature} = ${temperatureConverted}`);
+  const temperature = "-459F   ";
+  const temperatureConverted = degreeConvert(temperature);
+  console.log(`${temperature} = ${temperatureConverted}`);
 } catch (error) {
-    console.error(error);
+  console.error(error);
 }
