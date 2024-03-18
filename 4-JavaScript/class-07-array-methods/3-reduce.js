109e10312b8aec3bed970c4747c07eda743ba0f3
Eduardo Gonçalves <eduardogoncalves3693@gmail.com>
2024-03-18 12:33:12 -0300

    add comments and forEach

--- 4-JavaScript/class-07-array-methods/3-reduce.js
+++ 4-JavaScript/class-07-array-methods/3-reduce.js
@@ -1,29 +1,33 @@
-const ArrayNumbers = [2, 4, 6, 8, 10, 12];
+// variável ArrayNumbers
+const ArrayNumbers = [4, 6, 8, 10, 12, 14];
 
-const sumArrayNumbers = ArrayNumbers.reduce((prevValue,  currentValue) => {
+//?__________________
+// cria uma constante que soma o número do array presente com o valor passado
+const sumArrayNumbers = ArrayNumbers.reduce((prevValue, currentValue) => {
+  let count = 0;
 
-    let count = 0
-
-if (prevValue % 2 === 0) {
-    console.log("Entrouuu");
+  if (prevValue % 2 === 0) {
+    console.log("Entrouuu"); //
     count = prevValue = currentValue;
-}
-return count;
-})
-
-console.log(sumArrayNumbers);
-
+  }
+  return count;
+});
+console.log(sumArrayNumbers); // 14
+//?__________________
 
 const shoppingCart = [
-    {product: "Arroz", amount: 2, price: 20},
-    {product: "Café", amount: 3, price: 40},
-    {product: "Feijão", amount: 4, price: 60},
-]
-
+  { product: "Arroz", amount: 2, price: 20 },
+  { product: "Café", amount: 3, price: 40 },
+  { product: "Feijão", amount: 4, price: 60 },
+];
 
 const sumShoppingCart = shoppingCart.reduce((prevValue, currentValue) => {
-return prevValue + currentValue.price * currentValue.amount;
+  return prevValue + currentValue.price * currentValue.amount;
+  //? price - amount
+  // Arroz: Preço = $20, Quantidade = 2
+  //  Café: Preço = $40, Quantidade = 3
+  // Feijão:Preço = $60, Quantidade = 4
+  // Valor total = 40 + 120 + 240 = 400
 }, 0);
 
 console.log(sumShoppingCart);
-
