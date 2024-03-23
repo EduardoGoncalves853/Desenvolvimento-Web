72f845305e7ca7fca9ad662e7b3582c9011690ca
Eduardo Gonçalves <eduardogoncalves3693@gmail.com>
2024-03-22 21:10:35 -0300

    add create uuid, and password hash and read controller

--- 7-Node.js/class-04-databases/src/controllers/index.ts
+++ 7-Node.js/class-04-databases/src/controllers/index.ts
@@ -1,22 +1,52 @@
 import { Request, Response, NextFunction } from "express";
 import { sqliteConnection } from "../databases/sqlite3";
+import { randomUUID } from "node:crypto";
+import { compare, hash } from "bcrypt";
 
 export const userControllers = {
   async create(req: Request, res: Response, next: NextFunction) {
     try {
-      const { id, name, email, password } = req.body;
+      const { name, email, password } = req.body;
+      const uuid = randomUUID();
       const db = await sqliteConnection();
+      const passwordHash = await hash(password, 10);
       await db.run(
-        "INSERT INTO users (id ,name, email, password) VALUES (? ,?, ?,? )",
-        [id, name, email, password]
+        "INSERT INTO users (id ,name, email, password) VALUES (?, ?, ?, ?)",
+        [uuid, name, email, passwordHash]
       );
-      return res.json({ message: "created!", id });
+      return res.status(201).json({ message: "created!", id: uuid });
     } catch (error) {
       return next(error);
     }
   },
-  read(req: Request, res: Response) {
-    res.send({ message: "readed!" });
+  async read(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+      const { password } = req.body;
+      const db = await sqliteConnection();
+      // senha errada ↓
+      if (!password) {
+        throw res
+          .status(400)
+          .json({ message: "please confirm your password!" });
+      }
+      const user = await db.get("SELECT * FROM users WHERE id = ?;", [id]);
+      // usuário diferente ↓
+      if (!user) {
+        throw res.status(404).json({ message: "User not found" });
+      }
+      // checa a senha(true || false)
+      const passwordCheck = await compare(password, user.password);
+      if (!passwordCheck) {
+        throw res.status(401).json({ message: "invalid password!" });
+      }
+
+      const { name, email } = user;
+
+      return res.status(200).json({ name , email });
+    } catch (error) {
+      return next(error);
+    }
   },
   update(req: Request, res: Response) {
     res.send({ message: "updated!" });
