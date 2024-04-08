d68a06abf6c7bc8f08a07550acf6dbe7e6524495
Eduardo Gonçalves <eduardogoncalves3693@gmail.com>
2024-04-08 19:36:51 -0300

    add database connection

--- 7-Node.js/class-05-authentication/src/server.ts
+++ 7-Node.js/class-05-authentication/src/server.ts
@@ -3,6 +3,8 @@ import "dotenv/config";
 import { pageNotFound } from "./errors/pageNotFound";
 import { appErrors } from "./errors/appErros";
 import { routes } from "./routes";
+import { sqliteConnection } from "./databases/sqlite3";
+import { runMigration } from "./databases/sqlite3/migrations";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,3 +18,9 @@ app.use(appErrors);
 app.listen(PORT, () => {
   console.log(`Server is running in Ice Cold ${PORT}`);
 });
+
+sqliteConnection()
+.then(() => console.log("Database connectado..."))
+.catch((error) => console.error("Database não conectado", error));
+
+runMigration();
