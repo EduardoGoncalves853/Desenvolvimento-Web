e7e997416ecc40b6438714964665352637adc545
Eduardo Gonçalves <eduardogoncalves3693@gmail.com>
2024-04-09 20:45:03 -0300

    add jwt token and response cookie

--- 7-Node.js/class-05-authentication/src/controllers/authControllers.ts
+++ 7-Node.js/class-05-authentication/src/controllers/authControllers.ts
@@ -1,8 +1,52 @@
 import { Request, Response, NextFunction } from "express";
+import { userRepository } from "../repositories/userRepository";
+import { compare } from "bcrypt";
+import { z } from "zod";
+import { sign } from "jsonwebtoken";
 
 export const authControllers = {
   async login(req: Request, res: Response, next: NextFunction) {
     try {
+      const userSchema = z
+        .object({
+          email: z
+            .string({
+              invalid_type_error: "Somente texto!",
+            })
+            .email({ message: "Email com formato inválido!" })
+            .max(255, "limite máximo atingido"),
+
+          password: z
+            .string({
+              invalid_type_error: "Para a senha use o tipo textual!",
+            })
+            .min(7, { message: "Senha com mínimo de 7 caracteres " })
+            .max(255, "limite máximo atingido"),
+        })
+        .strict();
+
+      const { email, password } = userSchema.parse(req.body);
+
+      const user = await userRepository.getByEmail(email);
+      if (!user) {
+        throw res.status(401).json({ message: "invalid email!" });
+      }
+      const passwordCheck = await compare(password, user.password);
+      if (!passwordCheck) {
+        throw res.status(401).json({ message: "invalid password!" });
+      }
+
+      const token = sign({ id: user.id }, process.env.SECRET_TOKEN, {
+        expiresIn: process.env.EXPIRESIN_TOKEN,
+      });
+
+      res.cookie(process.env.KEY_TOKEN, token, {
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,
+        maxAge: 1000 * 60 * 15,
+      });
+
       return res.status(200).json({ message: "User logged in!" });
     } catch (error) {
       return next(error);
