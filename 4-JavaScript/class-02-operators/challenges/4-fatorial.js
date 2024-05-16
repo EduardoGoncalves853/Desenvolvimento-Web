715817bee6bc0f655657fb817ee344568c146a04
Eduardo Gonçalves <eduardogoncalves3693@gmail.com>
2024-05-15 22:10:55 -0300

    add fatorial sem recursão

--- 4-JavaScript/class-02-operators/challenges/4-fatorial.js
+++ 4-JavaScript/class-02-operators/challenges/4-fatorial.js
@@ -1,5 +1,20 @@
-const n = prompt("digite o numero no qual queira fatorar");
+// Fatorial sem recursão
+console.time("fatorial sem recursão");
 
+let An = [0];
+let lastAr = An[An.length - 1];
+
+while (lastAr > 1) {
+  lastAr -= 1;
+  An.push(lastAr);
+}
+
+console.log(An.reduce((total, valorAtual) => total * valorAtual, 1));
+
+console.timeEnd("fatorial sem recursão");
+
+// Fatorial com recursão
+console.time("fatorial com recursão");
 function fatorial(n) {
   if (n === 0 || n === 1) {
     return 1;
@@ -8,4 +23,5 @@ function fatorial(n) {
   }
 }
 
-console.log(`seu ${n}! é ${fatorial(n)}`);
+console.log(fatorial(1));
+console.timeEnd("fatorial com recursão");
--- 4-JavaScript/class-03-function/challenge/3-FormulaDeBhaskara.js
+++ 4-JavaScript/class-03-function/challenge/3-FormulaDeBhaskara.js
@@ -2,19 +2,16 @@ var a = prompt("Digite um numero para (a)");
 var b = prompt("Digite um numero para (b)");
 var c = prompt("Digite um numero para (c)");
 
-
 function bhaskara(a, b, c) {
-    var delta = b * b - 4 * a * c;
-    if (delta < 0) {
-        return "Delta é negativo, a equação não possui raízes reais.";
-    }
-    var x1 = (-b + Math.sqrt(delta)) / (2 * a);
-    var x2 = (-b - Math.sqrt(delta)) / (2 * a);
-    return [x1, x2];
-};
+  var delta = b * b - 4 * a * c;
+  if (delta < 0) {
+    return "Delta é negativo, a equação não possui raízes reais.";
+  }
+  var x1 = (-b + Math.sqrt(delta)) / (2 * a);
+  var x2 = (-b - Math.sqrt(delta)) / (2 * a);
+  return [x1, x2];
+}
 
 var resultado = bhaskara(a, b, c);
 
 console.log(resultado);
-
-
